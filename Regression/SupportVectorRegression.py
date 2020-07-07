0dc7eaebdb48be54ef660adecc6ecf68c0a4562e
Chak <chak.koppula199@gmail.com>
2020-07-07 04:23:02 +0100

    Uploading completed SVR file

--- Regression/SupportVectorRegression.py
+++ Regression/SupportVectorRegression.py
@@ -1,23 +1,59 @@
+"""
+Support vector regression model to predict the previous salary of an applicant
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.svm import SVR
-from sklearn.preprocessing import StandardScalar
+from sklearn.preprocessing import StandardScaler
 
 """
 ---------------Data Pre-processing---------------
 """
-
 # Importing dataset
 dataset = pd.read_csv("../Data/Regression/Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 # Convert y to a vector
-y = t.reshape(len(y), 1)
+y = y.reshape(len(y), 1)
 
 # Feature Scaling
-sc_X = StandardScalar()
-sc_y = StandardScalar()
+sc_X = StandardScaler()
+sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
+
+"""
+---------------Training a Support Vecotr Regression Model---------------
+"""
+# Uses Radial Basis Function
+regressor = SVR(kernel="rbf")
+regressor.fit(X, y)
+
+"""
+---------------Predicting Results---------------
+"""
+value_to_predict = 6.5
+scaled_value = sc_X.transform([[value_to_predict]])
+print(sc_y.inverse_transform(regressor.predict(scaled_value)))
+
+"""
+---------------Visualising Support Vector Regression Results---------------
+"""
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X)), color='blue')
+plt.title("Support Vector Regression Results")
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid))), color='blue')
+plt.title("Support Vector Regression Results")
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
