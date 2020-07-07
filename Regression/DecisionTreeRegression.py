10f24af3d445a9656d6334f48334fecddcf7e347
Chak <chak.koppula199@gmail.com>
2020-07-07 18:06:26 +0100

    Adding Decision Tree Regressor

--- Regression/DecisionTreeRegression.py
+++ Regression/DecisionTreeRegression.py
@@ -1,10 +1,13 @@
 """
 Decision tree regression model to predict the previous salary of an applicant
+Model performs poorly as the data only contains a single feature and this model
+is better suited to multi-feature data sets.
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 """
 ---------------Data Pre-processing---------------
@@ -17,3 +20,26 @@ dataset = pd.read_csv("../Data/Regression/Position_Salaries.csv")
 # Exclude first column
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+
+"""
+---------------Training a Decision Tree Regression Model---------------
+"""
+regressor = DecisionTreeRegressor(random_state=42)
+regressor.fit(X, y)
+
+"""
+---------------Predicting Results---------------
+"""
+print(regressor.predict([[6.5]]))
+
+"""
+---------------Visualising Support Vector Regression Results---------------
+"""
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title("Decision Tree Regression Results")
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
