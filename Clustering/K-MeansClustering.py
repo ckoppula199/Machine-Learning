594e137e44a9a7043b6fe76b82d432a754de1a45
Chak <chak.koppula199@gmail.com>
2020-07-24 02:08:02 +0100

    Used elbow method to find out the optimum number of clusters to use (5)

--- Clustering/K-MeansClustering.py
+++ Clustering/K-MeansClustering.py
@@ -5,6 +5,7 @@ K-Means Clustering model to determine patterns in customer data
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 """
 ---------------Data Pre-processing---------------
@@ -19,3 +20,20 @@ X = dataset.iloc[:, 3:].values
 """
 ---------------Using 'elbow method' to find the optimal number of clusters---------------
 """
+# Within Cluster Sum of Squares used as metric to find optimal number of clusters
+wcss = []
+# Testing with upto 10 clusters
+for num_clusters in range(1, 11):
+    # k-means++ avoids random initialisation trap
+    kmeans = KMeans(n_clusters = num_clusters, init='k-means++', random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+# Plot the graph of wcss against cluster number
+plt.plot(range(1, 11), wcss)
+plt.title("Elboe Method")
+plt.xlabel("Number of Clusters")
+plt.ylabel("Within Cluster Sum of Squares")
+plt.show()
+
+# Graph shows that 5 clusters is the optimal amount
