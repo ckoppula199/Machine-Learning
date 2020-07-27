b6ad9834c0bac43958caa65977c50c0c881d3e53
Chak <chak.koppula199@gmail.com>
2020-07-27 01:09:20 +0100

    Trained AgglomerativeClustering model on the data and visualised the results

--- Clustering/Hierarchical_Clustering.py
+++ Clustering/Hierarchical_Clustering.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.cluster.hierarchy as sch
+from sklearn.cluster import AgglomerativeClustering
 
 """
 ---------------Data Pre-processing---------------
@@ -33,3 +34,25 @@ plt.show()
 """
 ---------------Training the Hierarchical Clustering Model on the Dataset---------------
 """
+# affinty is the type of distance used to measure distance between clusters
+# linkage='ward' means minimum variance used as the clustering technique
+hc = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
+y_pred = hc.fit_predict(X)
+
+
+"""
+---------------Visualising the Cluster Predictions---------------
+"""
+plt.figure(figsize=(16,8))
+# Select rows such that only points plotted are the ones assigned to the same cluster
+plt.scatter(X[y_pred==0,0], X[y_pred==0,1], s=100, color='red', label='Cluster 1')
+plt.scatter(X[y_pred==1,0], X[y_pred==1,1], s=100, color='blue', label='Cluster 2')
+plt.scatter(X[y_pred==2,0], X[y_pred==2,1], s=100, color='green', label='Cluster 3')
+plt.scatter(X[y_pred==3,0], X[y_pred==3,1], s=100, color='cyan', label='Cluster 4')
+plt.scatter(X[y_pred==4,0], X[y_pred==4,1], s=100, color='magenta', label='Cluster 5')
+
+plt.title("Clusters of Customers")
+plt.xlabel("Anual Income ($)")
+plt.ylabel("Spending Score (1-100)")
+plt.legend()
+plt.show()
