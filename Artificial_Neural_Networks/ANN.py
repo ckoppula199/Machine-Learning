12fc62ddbdadb41d6f6818123b779b58affaf115
Chak <chak.koppula199@gmail.com>
2020-08-14 20:43:08 +0100

    evaluated performance of ANN on the dataset

--- Artificial_Neural_Networks/ANN.py
+++ Artificial_Neural_Networks/ANN.py
@@ -11,6 +11,9 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 
 """
 ---------------Data Pre-processing---------------
@@ -49,6 +52,7 @@ Initialising the Aritificial Neural Network
 ann = tf.keras.models.Sequential()
 # Add input layer and first hidden layer
 ann.add(tf.keras.layers.Dense(units=6, activation='relu', input_shape=(X_train.shape[1],)))
+# ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 # Add second hidden layer
 ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 # Add output layer
@@ -62,3 +66,40 @@ ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Training the ANN on the training set
 ann.fit(X_train, y_train, batch_size=32, epochs=100)
+
+"""
+Making Predictions and Evaluating the Model
+"""
+# Predicting a single result
+# Predict Geography: France, Credit Score: 600, Gender: Male, Age: 40, Tenure: 3 years,
+# Balance: 60000, Number of Products: 2, Has Credit Card?: Yes, Is Active Member?: Yes,
+# Estimated Salary: 50000
+leave_prob = ann.predict(sc.transform([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]]))
+if leave_prob > 0.5:
+    print("Customer is likely to leave the bank")
+else:
+    print("Customer is unlikely to leave the bank")
+
+# Use model on the Test set
+y_pred = ann.predict(X_test)
+# Boolean list of whether or not customer is likely to leave
+y_pred = (y_pred > 0.5)
+# Convert vectors from horizontal to vertical and display side by side to compare
+# print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), axis=1))
+
+"""
+--------------Making the Confusion Matrix and Evaluating the Model---------------
+"""
+
+# Gives confusion matrix C such that Cij is equal to the number of observations known to be in group i and predicted to be in group j.
+cm = confusion_matrix(y_test, y_pred)
+true_positive = cm[1][1]
+true_negative = cm[0][0]
+false_positive = cm[0][1]
+false_negative = cm[1][0]
+
+print("\nModel Evaluation")
+print(f"Accuracy is {accuracy_score(y_test, y_pred)}")
+print(f"Precision is {true_positive / (true_positive + false_positive)}")
+print(f"Recall is {true_positive / (true_positive + false_negative)}")
+print(f"F1 score is {f1_score(y_test, y_pred, average='binary')}")
